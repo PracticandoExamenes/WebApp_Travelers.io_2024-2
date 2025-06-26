6a30f48dcbba63fb0affbf2a63947686cc166cd4
Paolo <u20221f613@upc.edu.pe>
2025-06-25 21:57:10 -0500

    feat(monetizationStrategy): add value object

---
+++ si732pc2u20221f613.API/Subscriptions/domain/model/valueobjects/EMonetizationStrategy.cs
@@ -0,0 +1,9 @@
+﻿namespace si732pc2u20221f613.API.Subscriptions.domain.model.valueobjects;
+
+public enum EMonetizationStrategy
+{
+    MonthlySubscription = 1,
+    YearlySubscription = 2,
+    PerTransactionFee = 3,
+    OneTimePurchase = 4
+}
